0e3ba65555228ead7cc470727949fc0279d6c09d
Chance Harmon <henrikillsit@gmail.com>
2021-03-23 11:17:48 -0700

    weather setup for fake data

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const weatherObj = require('./data/weather.json');
 
 const PORT = process.env.PORT;
 
 app.use(cors());
 
+app.get('/weather', handleWeather);
 
+function handleWeather(request, response) {
+  let forecastArray = weatherObj.data.map(day => {
+    return new Weather(day)
+  })
+  response.status(200).send(forecastArray)
+}
+
+function Weather(obj) {
+  this.description = obj.weather.description;
+  this.date = obj.datetime;
+}
 
 
 app.listen(PORT, () => {
